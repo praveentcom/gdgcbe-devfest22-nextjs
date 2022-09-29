a59ef9decea35296fc7c78ceffd36e740157db81
Ramakrishnan Kandasamy <ramky24@gmail.com>
2022-09-29 16:31:42 +0530

    Add Schedule

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 
+import Script from 'next/script'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
@@ -9,6 +10,21 @@ import { Speakers } from '@/components/Speakers'
 export default function Home() {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-5TH4SMY576`}
+      />
+
+      <Script id="nn" strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-5TH4SMY576', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
       <Head>
         <title>GDG Coimbatore DevFest 2022</title>
         <meta
